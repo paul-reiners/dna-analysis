404921ad77e917645633fdbb84fca64876a0d833
Paul Reiners <paul.reiners@gmail.com>
2020-06-06 16:46:33 -0500

    Finished test_get_pattern_matches.

--- src/tests/chapter02/test_pattern.py
+++ src/tests/chapter02/test_pattern.py
@@ -1,4 +1,4 @@
-from chapter02.pattern import get_reverse_complement
+from chapter02.pattern import get_reverse_complement, get_pattern_matches
 
 
 def test_get_reverse_complement():
@@ -14,3 +14,11 @@ def test_get_reverse_complement_2():
     pattern = lines[0].strip()
     computed_output = get_reverse_complement(pattern)
     print(computed_output)
+
+
+def test_get_pattern_matches():
+    pattern = 'ATAT'
+    genome = 'GATATATGCATATACTT'
+    computed_output = get_pattern_matches(pattern, genome)
+    expected_output = [1, 3, 9]
+    assert computed_output == expected_output
