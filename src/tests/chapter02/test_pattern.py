3dfddb81e3deea72dcc4ccc269a9722826473781
Paul Reiners <paul.reiners@gmail.com>
2020-06-06 16:39:50 -0500

    Finished test_get_reverse_complement_2.

--- src/tests/chapter02/test_pattern.py
+++ src/tests/chapter02/test_pattern.py
@@ -6,3 +6,11 @@ def test_get_reverse_complement():
     computed_output = get_reverse_complement(pattern)
     expected_output = 'ACCGGGTTTT'
     assert computed_output == expected_output
+
+
+def test_get_reverse_complement_2():
+    file1 = open('../../../data/chapter01/dataset_3_2.txt', 'r')
+    lines = file1.readlines()
+    pattern = lines[0].strip()
+    computed_output = get_reverse_complement(pattern)
+    print(computed_output)
