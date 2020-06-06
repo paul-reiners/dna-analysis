92a7d461bb07a03d78cb26efdf59ab4841383152
Paul Reiners <paul.reiners@gmail.com>
2020-06-06 15:05:33 -0500

    Finished test_neighbors_3.

--- src/phix174/test_phiX174_error_free_overlap.py
+++ src/phix174/test_phiX174_error_free_overlap.py
@@ -7,3 +7,12 @@ def test_pattern_count():
 
 def test_pattern_count_2():
     assert pattern_count('GCGCG', 'GCG') == 2
+
+
+def test_neighbors_3():
+    file1 = open('../../data/chapter01/dataset_2_7.txt', 'r')
+    lines = file1.readlines()
+    text = lines[0].strip()
+    pattern = lines[1].strip()
+    computed_output = pattern_count(text, pattern)
+    assert computed_output == 24
