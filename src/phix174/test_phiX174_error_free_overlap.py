afda710e927767932f115011a4207b20bb347900
Paul Reiners <paul.reiners@gmail.com>
2020-06-06 15:00:41 -0500

    Finished test_pattern_count_2.

--- src/phix174/test_phiX174_error_free_overlap.py
+++ src/phix174/test_phiX174_error_free_overlap.py
@@ -2,4 +2,8 @@ from phix174.phiX174_error_free_overlap import pattern_count
 
 
 def test_pattern_count():
-    assert pattern_count('ACAACTATGCATACTATCGGGAACTATCCT', 'ACTAT') == 3.
+    assert pattern_count('ACAACTATGCATACTATCGGGAACTATCCT', 'ACTAT') == 3
+
+
+def test_pattern_count_2():
+    assert pattern_count('GCGCG', 'GCG') == 2
