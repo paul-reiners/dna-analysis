56834b7b817747afed89cb41035ca7b4d090a45f
Paul Reiners <paul.reiners@gmail.com>
2020-06-06 16:13:27 -0500

    Finished test_frequent_words_2.

--- src/phix174/test_phiX174_error_free_overlap.py
+++ src/phix174/test_phiX174_error_free_overlap.py
@@ -24,3 +24,13 @@ def test_frequent_words():
     computed_output = frequent_words(text, k)
     expected_output = ['CATG', 'GCAT']
     assert sorted(computed_output) == sorted(expected_output)
+
+
+def test_frequent_words_2():
+    file1 = open('../../data/chapter01/dataset_2_10.txt', 'r')
+    lines = file1.readlines()
+    text = lines[0].strip()
+    k = int(lines[1].strip())
+    computed_output = frequent_words(text, k)
+    expected_output = ['GCACGGTATTGC', 'TTGCACGGTATT']
+    assert sorted(computed_output) == sorted(expected_output)
