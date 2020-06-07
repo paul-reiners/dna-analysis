015dc10287d268875612dfc17ef681261a6083c5
Paul Reiners <paul.reiners@gmail.com>
2020-06-07 14:47:29 -0500

    Finished number_to_pattern.

--- src/tests/chapters01/test_chapter01.py
+++ src/tests/chapters01/test_chapter01.py
@@ -1,5 +1,9 @@
-from chapter01.chapter01 import pattern_to_number
+from chapter01.chapter01 import pattern_to_number, number_to_pattern
 
 
 def test_pattern_to_number():
     assert pattern_to_number('GT') == 11
+
+
+def test_number_to_pattern():
+    assert number_to_pattern(11, 2) == 'GT'
