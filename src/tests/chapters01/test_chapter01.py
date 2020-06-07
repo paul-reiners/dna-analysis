8f508b804976b29b2704cb3920a6b49a76396481
Paul Reiners <paul.reiners@gmail.com>
2020-06-07 14:50:44 -0500

    Finished test_pattern_to_number_2.

--- src/tests/chapters01/test_chapter01.py
+++ src/tests/chapters01/test_chapter01.py
@@ -5,5 +5,11 @@ def test_pattern_to_number():
     assert pattern_to_number('GT') == 11
 
 
+def test_pattern_to_number_2():
+    actual_result = pattern_to_number('ATGCAA')
+    expected_result = 912
+    assert actual_result == expected_result
+
+
 def test_number_to_pattern():
     assert number_to_pattern(11, 2) == 'GT'
