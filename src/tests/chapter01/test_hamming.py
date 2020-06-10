0e28ccc19066c502bdbb649716d5d50f081fecd6
Paul Reiners <paul.reiners@gmail.com>
2020-06-10 18:18:32 -0500

    Wrote test_find_approximate_pattern_matches.

--- src/tests/chapter01/test_hamming.py
+++ src/tests/chapter01/test_hamming.py
@@ -1,4 +1,4 @@
-from chapter01.hamming import hamming_distance
+from chapter01.hamming import hamming_distance, find_approximate_pattern_matches
 
 
 def test_hamming_distance():
@@ -6,4 +6,22 @@ def test_hamming_distance():
     s2 = 'GGACCGTTGAC'
     computed_result = hamming_distance(s1, s2)
     expected_result = 3
+    assert computed_result == expected_result
+
+
+def test_hamming_distance_2():
+    file1 = open('../../../data/chapter01/dataset_9_3.txt', 'r')
+    lines = file1.readlines()
+    s1 = lines[0].strip()
+    s2 = lines[1].strip()
+    calculated_output = hamming_distance(s1, s2)
+    print(calculated_output)
+
+
+def test_find_approximate_pattern_matches():
+    pattern = 'ATTCTGGA'
+    text = 'CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAAT = 3'
+    d = 3
+    computed_result = find_approximate_pattern_matches(pattern, text, d)
+    expected_result = [6, 7, 26, 27]
     assert computed_result == expected_result
