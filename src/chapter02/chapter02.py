c635440cf77424e9d2b32441f2d9c4edfcbf18fa
Paul Reiners <paul.reiners@gmail.com>
2020-06-10 19:43:34 -0500

    Wrote frequent_words_with_mismatches_and_reverse_complements.

--- src/chapter02/chapter02.py
+++ src/chapter02/chapter02.py
@@ -1,6 +1,7 @@
 from itertools import product
 
 from chapter01.hamming import count
+from chapter01.pattern import get_reverse_complement
 
 
 def get_all_k_mers(k):
@@ -14,6 +15,26 @@ def frequent_words_with_mismatches(text, k, d):
     for k_mer in k_mers:
         k_mer_str = ''.join(k_mer)
         n = count(text, k_mer_str, d)
+        if n > max_count:
+            max_count = n
+        k_mer_to_freq[k_mer_str] = n
+    frequent_words = []
+    for k in k_mer_to_freq:
+        n = k_mer_to_freq[k]
+        if n == max_count:
+            frequenwt_words.append(k)
+
+    return frequent_words
+
+
+def frequent_words_with_mismatches_and_reverse_complements(text, k, d):
+    k_mers = get_all_k_mers(k)
+    k_mer_to_freq = {}
+    max_count = -1
+    for k_mer in k_mers:
+        k_mer_str = ''.join(k_mer)
+        reverse_complement = get_reverse_complement(k_mer_str)
+        n = count(text, k_mer_str, d) + count(text, reverse_complement, d)
         if n > max_count:
             max_count = n
         k_mer_to_freq[k_mer_str] = n
--- src/tests/chapter02/test_chapter02.py
+++ src/tests/chapter02/test_chapter02.py
@@ -1,4 +1,4 @@
-from chapter02.chapter02 import frequent_words_with_mismatches
+from chapter02.chapter02 import frequent_words_with_mismatches, frequent_words_with_mismatches_and_reverse_complements
 
 
 def test_frequent_words_with_mismatches():
@@ -7,4 +7,24 @@ def test_frequent_words_with_mismatches():
     d = 1
     computed_result = frequent_words_with_mismatches(text, k, d)
     expected_result = ['GATG', 'ATGC', 'ATGT']
+    assert sorted(computed_result) == sorted(expected_result)
+
+
+def test_frequent_words_with_mismatches_2():
+    file1 = open('../../../data/chapter02/dataset_9_7.txt', 'r')
+    lines = file1.readlines()
+    text = lines[0].strip()
+    k_and_d = lines[1].strip().split()
+    k = int(k_and_d[0])
+    d = int(k_and_d[1])
+    calculated_output = frequent_words_with_mismatches(text, k, d)
+    print(' '.join(map(lambda n: str(n), calculated_output)))
+
+
+def test_frequent_words_with_mismatches_and_reverse_complements():
+    text = 'ACGTTGCATGTCGCATGATGCATGAGAGCT'
+    k = 4
+    d = 1
+    computed_result = frequent_words_with_mismatches_and_reverse_complements(text, k, d)
+    expected_result = ['ATGT', 'ACAT']
     assert sorted(computed_result) == sorted(expected_result)
