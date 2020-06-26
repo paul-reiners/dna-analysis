4974c5d8059883bf9168aa0ccdaf440dcf432895
Paul Reiners <paul.reiners@gmail.com>
2020-06-26 15:51:52 -0500

    Wrote test_composition_2.

--- src/tests/bioinformatics_ii/test_week1.py
+++ src/tests/bioinformatics_ii/test_week1.py
@@ -7,3 +7,14 @@ def test_composition():
     calculated_result = composition(text, k)
     expected_result = ['CAATC', 'AATCC', 'ATCCA', 'TCCAA', 'CCAAC']
     assert calculated_result == sorted(expected_result)
+
+
+def test_composition_2():
+    file1 = open('../../../data/bioinformatics_ii/week1/dataset_197_3.txt', 'r')
+    lines = file1.readlines()
+    k = int(lines[0].strip())
+    text = lines[1].strip()
+    calculated_result = composition(text, k)
+    print('\n'.join(calculated_result))
+    with open('../../../data/bioinformatics_ii/week1/dataset_197_3-OUTPUT.txt', 'w') as f:
+        print('\r\n'.join(calculated_result), file=f, flush=True)
