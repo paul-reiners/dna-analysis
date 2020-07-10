7b97461a8cfdbf4d4443f9160f0ed3a6622b1475
Paul Reiners <paul.reiners@gmail.com>
2020-07-10 18:37:46 -0500

    Added test case.

--- src/tests/bioinformatics_ii/test_week2.py
+++ src/tests/bioinformatics_ii/test_week2.py
@@ -17,3 +17,22 @@ def test_get_eulerian_cycle():
             break
 
     assert found
+
+
+def test_get_eulerian_cycle_2():
+    file1 = open('../../../data/bioinformatics_ii/week1/dataset_203_2.txt', 'r')
+    lines = file1.readlines()
+    lines = [line.strip() for line in lines]
+    graph = {}
+    for line in lines:
+        key_and_value = line.split("->")
+        key = int(key_and_value[0])
+        value_strs = key_and_value[1].split(',')
+        values = [int(s) for s in value_strs]
+        graph[key] = values
+    calculated_result = get_eulerian_cycle(graph)
+    out_f = open('../../../data/bioinformatics_ii/week1/dataset_203_2_output.txt', 'w')
+    calculated_result.append(calculated_result[0])
+    output_line = '->'.join([str(n) for n in calculated_result])
+    out_f.write(output_line)
+    out_f.write("\n")
