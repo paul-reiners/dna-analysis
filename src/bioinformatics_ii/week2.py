5824f429a79bf049d5a7aa495ffbe7937ba6d42d
Paul Reiners <paul.reiners@gmail.com>
2020-07-10 17:44:08 -0500

    Finished get_eulerian_cycle(graph).

--- src/bioinformatics_ii/week2.py
+++ src/bioinformatics_ii/week2.py
@@ -1,17 +1,6 @@
 import random
 
 
-def get_random_cycle(graph):
-    random_cycle = []
-    nodes = list(graph.keys())
-    node = random.choice(nodes)
-    while node not in random_cycle:
-        random_cycle.append(node)
-        nodes = graph[node]
-        unseen_nodes = [n for n in nodes if n not in random_cycle[1:] or n == random_cycle[0]]
-        node = random.choice(unseen_nodes)
-    return random_cycle
-
 def get_all_edges(graph):
     return [[i, j] for i in graph.keys() for j in graph[i]]
 
@@ -23,20 +12,62 @@ def get_unexplored_edges(unexplored_edges, cycle):
     return unexplored_edges
 
 
-def get_new_start(cycle, unexplored_edges):
-    new_starts = [edge for edge in unexplored_edges if edge[0] in cycle]
+def get_next_cycle(unexplored_edges):
+    next_cycle = []
+    nodes = [e[0] for e in unexplored_edges]
+    node = random.choice(nodes)
+    while node not in next_cycle:
+        next_cycle.append(node)
+        nodes = [e[1] for e in unexplored_edges if e[0] == next_cycle[-1]]
+        node = random.choice(nodes)
+    start = next_cycle.index(node)
+    return next_cycle[start:]
 
-    return new_starts[0]
+
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+
+def rotate(cycle, common_node):
+    idx = cycle.index(common_node)
+
+    return cycle[idx:] + cycle[:idx]
+
+
+def splice_all_cycles(cycles):
+    n = len(cycles)
+    while n > 1:
+        trim_cycles(cycles, n)
+        n = len(cycles)
+
+    return cycles[0]
+
+
+def trim_cycles(cycles, n):
+    for i in range(n):
+        cycle1 = cycles[i]
+        for j in range(i + 1, n):
+            cycle2 = cycles[j]
+            intersect = intersection(cycle1, cycle2)
+            if len(intersect) > 0:
+                common_node = intersect[0]
+                rotated_cycle1 = rotate(cycle1, common_node)
+                pos = cycle2.index(common_node)
+                spliced_cycle = cycle2[:pos] + rotated_cycle1[:] + cycle2[pos:]
+                cycles.append(spliced_cycle)
+                cycles.remove(cycle1)
+                cycles.remove(cycle2)
+
+                return cycles
 
 
 def get_eulerian_cycle(graph):
     unexplored_edges = get_all_edges(graph)
-    cycle = get_random_cycle(graph)
-    unexplored_edges = get_unexplored_edges(unexplored_edges, cycle)
+    cycles = []
     while len(unexplored_edges) > 0:
-        new_start = get_new_start(cycle, unexplored_edges)
-        next_cycle = get_random_cycle(graph, cycle, unexplored_edges)
-        cycle = next_cycle
-        unexplored_edges = get_unexplored_edges(unexplored_edges, cycle)
+        next_cycle = get_next_cycle(unexplored_edges)
+        cycles.append(next_cycle)
+        unexplored_edges = get_unexplored_edges(unexplored_edges, next_cycle)
+    eulerian_cycle = splice_all_cycles(cycles)
 
-    return cycle
+    return eulerian_cycle
