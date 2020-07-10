e51c69ba5f2bd9aa4846c06d6084844dfb9e8eec
Paul Reiners <paul.reiners@gmail.com>
2020-07-10 15:19:17 -0500

    Simplified get_random_cycle(graph).

--- src/bioinformatics_ii/week2.py
+++ src/bioinformatics_ii/week2.py
@@ -8,8 +8,9 @@ def get_random_cycle(graph):
     while node not in random_cycle:
         random_cycle.append(node)
         nodes = graph[node]
-        node = random.choice(nodes)
-    return random_cycle[random_cycle.index(node):]
+        unseen_nodes = [n for n in nodes if n not in random_cycle[1:]]
+        node = random.choice(unseen_nodes)
+    return random_cycle
 
 def get_all_edges(graph):
     return [[i, j] for i in graph.keys() for j in graph[i]]
@@ -23,14 +24,16 @@ def get_unexplored_edges(unexplored_edges, cycle):
 
 
 def get_new_start(cycle, unexplored_edges):
-    pass
+    new_starts = filter(lambda unexplored_edge: unexplored_edge[0] in cycle, unexplored_edges)
+
+    return new_starts[0]
 
 
 def get_eulerian_cycle(graph):
     unexplored_edges = get_all_edges(graph)
     cycle = get_random_cycle(graph)
     unexplored_edges = get_unexplored_edges(unexplored_edges, cycle)
-    while len(unexplored_edges > 0):
+    while len(unexplored_edges) > 0:
         new_start = get_new_start(cycle, unexplored_edges)
         next_cycle = get_random_cycle(graph, cycle, unexplored_edges)
         cycle = next_cycle
