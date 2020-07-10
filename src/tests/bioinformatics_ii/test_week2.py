8650f67cdff05f076e7375792aa5e650da864a57
Paul Reiners <paul.reiners@gmail.com>
2020-07-10 17:56:27 -0500

    Finished test_get_eulerian_cycle.

--- src/tests/bioinformatics_ii/test_week2.py
+++ src/tests/bioinformatics_ii/test_week2.py
@@ -5,8 +5,15 @@ from collections import deque
 def test_get_eulerian_cycle():
     graph = {0: [3], 1: [0], 2: [1, 6], 3: [2], 4: [2], 5: [4], 6: [5, 8], 7: [9], 8: [7], 9: [6]}
     computed_result = get_eulerian_cycle(graph)
-    expected_result = deque([6, 8, 7, 9, 6, 5, 4, 2, 1, 0, 3, 2])
+    expected_result = [6, 8, 7, 9, 6, 5, 4, 2, 1, 0, 3, 2]
     n = len(expected_result)
     assert len(computed_result) == n
-    rotations = [expected_result.rotate(i) for i in range(n)]
-    assert computed_result in rotations
+    found = False
+    for i in range(n):
+        rotation =  expected_result[i:] + expected_result[:i]
+        if rotation == computed_result:
+            found = True
+
+            break
+
+    assert found
