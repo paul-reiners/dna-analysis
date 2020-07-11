a56d2895b4863f1c0952743f7e3d7eefbde60b6a
Paul Reiners <paul.reiners@gmail.com>
2020-07-11 16:57:11 -0500

    Cleaned up unit tests.

--- src/tests/bioinformatics_i/week02/test_skew.py
+++ src/tests/bioinformatics_i/week02/test_skew.py
@@ -1,4 +1,4 @@
-from week02.skew import get_minimum_skew_positions
+from bioinformatics_i.week02.skew import get_minimum_skew_positions
 
 
 def test_get_minimum_skew_positions():
@@ -6,11 +6,3 @@ def test_get_minimum_skew_positions():
     computed_value = get_minimum_skew_positions(genome)
     expected_value = [11, 24]
     assert computed_value == expected_value
-
-
-def test_get_minimum_skew_positions_2():
-    file1 = open('../../../data/week02/dataset_7_6.txt', 'r')
-    lines = file1.readlines()
-    genome = lines[0].strip()
-    calculated_output = get_minimum_skew_positions(genome)
-    print(' '.join(map(lambda n: str(n), calculated_output)))
