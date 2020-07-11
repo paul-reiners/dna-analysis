9bc207ecf8b2f3629b28d6f981c9198b965f7618
Paul Reiners <paul.reiners@gmail.com>
2020-07-11 16:44:17 -0500

    Cleaned up unit tests.

--- src/tests/bioinformatics_i/week01/test_neighbors.py
+++ src/tests/bioinformatics_i/week01/test_neighbors.py
@@ -1,4 +1,4 @@
-from week02.neighbors import neighbors
+from bioinformatics_i.week01.neighbors import neighbors
 
 
 def test_neighbors():
@@ -7,12 +7,3 @@ def test_neighbors():
     computed_output = neighbors(pattern, d)
     expected_output = ['CCG', 'TCG', 'GCG', 'AAG', 'ATG', 'AGG', 'ACA', 'ACC', 'ACT', 'ACG']
     assert sorted(computed_output) == sorted(expected_output)
-
-
-def test_neighbors_2():
-    file1 = open('../../data/chapter02/dataset_3014_4.txt', 'r')
-    lines = file1.readlines()
-    pattern = lines[0].strip()
-    d = int(lines[1])
-    computed_output = neighbors(pattern, d)
-    print(' '.join(sorted(computed_output)))
