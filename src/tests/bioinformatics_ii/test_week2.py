af72ecba207ccbb35b627be1de1d9ff7081e4d65
Paul Reiners <paul.reiners@gmail.com>
2020-07-11 11:34:37 -0500

    Got rid of submission test sets.

--- src/tests/bioinformatics_ii/test_week2.py
+++ src/tests/bioinformatics_ii/test_week2.py
@@ -18,25 +18,6 @@ def test_get_eulerian_cycle():
     assert found
 
 
-def test_get_eulerian_cycle_2():
-    file1 = open('../../../data/bioinformatics_ii/week1/dataset_203_2.txt', 'r')
-    lines = file1.readlines()
-    lines = [line.strip() for line in lines]
-    graph = {}
-    for line in lines:
-        key_and_value = line.split("->")
-        key = int(key_and_value[0])
-        value_strs = key_and_value[1].split(',')
-        values = [int(s) for s in value_strs]
-        graph[key] = values
-    calculated_result = get_eulerian_cycle(graph)
-    out_f = open('../../../data/bioinformatics_ii/week1/dataset_203_2_output.txt', 'w')
-    calculated_result.append(calculated_result[0])
-    output_line = '->'.join([str(n) for n in calculated_result])
-    out_f.write(output_line)
-    out_f.write("\n")
-
-
 def test_get_eulerian_path():
     graph = {0: [2], 1: [3], 2: [1], 3: [0, 4], 6: [3, 7], 7: [8], 8: [9], 9: [6]}
     computed_result = get_eulerian_path(graph)
